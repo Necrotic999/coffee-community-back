bbbe9e8d4125178c880200f22ac6f1b9393fecea
Necrotic999 <l.ilya041105@gmail.com>
2025-12-04 01:05:54 +0200

    add logs

--- controllers/vacanciesControllers.js
+++ controllers/vacanciesControllers.js
@@ -15,6 +15,7 @@ const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
 
 export const sendEmail = async (req, res, next) => {
   try {
+    console.log("1. Запрос пришёл на бэкенд", new Date().toISOString());
     const { name, surname, phoneNumber, recaptchaToken } = req.body;
 
     const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
@@ -31,12 +32,19 @@ export const sendEmail = async (req, res, next) => {
       throw HttpError(400, "Виглядаєте як робот, спробуйте ще раз");
     }
 
+    console.log(
+      "2. reCAPTCHA пройдена, начинаем отправку письма",
+      new Date().toISOString()
+    );
+
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: process.env.TARGET_EMAIL,
       subject: "Нова заявка",
       text: `Новий претендент на вакансію: ${name} ${surname}\nНомер телефону: ${phoneNumber}`,
     });
+
+    console.log("3. Письмо успешно отправлено", new Date().toISOString());
 
     return res.status(201).json({
       success: true,
