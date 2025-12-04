9283df01895293cb40a62400111c7acb4bd9dea6
Necrotic999 <l.ilya041105@gmail.com>
2025-12-04 11:56:03 +0200

    change gmail service to brevo

--- controllers/vacanciesControllers.js
+++ controllers/vacanciesControllers.js
@@ -4,7 +4,9 @@ import "dotenv/config";
 import HttpError from "../helpers/HttpError.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -15,7 +17,6 @@ const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
 
 export const sendEmail = async (req, res, next) => {
   try {
-    console.log("1. Запрос пришёл на бэкенд", new Date().toISOString());
     const { name, surname, phoneNumber, recaptchaToken } = req.body;
 
     const verifyUrl = `https://www.google.com/recaptcha/api/siteverify`;
@@ -32,19 +33,12 @@ export const sendEmail = async (req, res, next) => {
       throw HttpError(400, "Виглядаєте як робот, спробуйте ще раз");
     }
 
-    console.log(
-      "2. reCAPTCHA пройдена, начинаем отправку письма",
-      new Date().toISOString()
-    );
-
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: process.env.TARGET_EMAIL,
       subject: "Нова заявка",
       text: `Новий претендент на вакансію: ${name} ${surname}\nНомер телефону: ${phoneNumber}`,
     });
-
-    console.log("3. Письмо успешно отправлено", new Date().toISOString());
 
     return res.status(201).json({
       success: true,
